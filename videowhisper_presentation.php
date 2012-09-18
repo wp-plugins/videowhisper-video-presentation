048b32b49741191788876c30ebb18217d297f08a
videowhisper <videowhisper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-18 23:56:38 +0000

    3.25
    git-svn-id: https://plugins.svn.wordpress.org/videowhisper-video-presentation/trunk@600781 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- videowhisper_presentation.php
+++ videowhisper_presentation.php
@@ -3,7 +3,7 @@
 Plugin Name: VideoWhisper Video Presentation
 Plugin URI: http://www.videowhisper.com/?p=WordPress+Video+Presentation
 Description: Video Presentation
-Version: 3.17	
+Version: 3.25	
 Author: VideoWhisper.com
 Author URI: http://www.videowhisper.com/
 Contributors: videowhisper, VideoWhisper.com
